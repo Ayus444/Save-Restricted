f34adcb4b2ea83dd72c7ab3016a92c8e933e5779
Ayush <112577569+Ayus444@users.noreply.github.com>
2025-04-23 21:42:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 # Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client
+from pyrogram.errors import FloodWait
+import asyncio
 from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
@@ -18,19 +20,17 @@ class Bot(Client):
             sleep_threshold=10
         )
 
-      
-    async def start(self):
-            
-        await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+    async def start(self, **kwargs):  # Accept extra args like use_qr
+        try:
+            await super().start(**kwargs)
+            print('Bot Started Powered By @VJ_Botz')
+        except FloodWait as e:
+            print(f"FloodWait: Sleeping for {e.value} seconds...")
+            await asyncio.sleep(e.value)
+            await self.start(**kwargs)
 
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
 Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
